18868bd090586ddaa9014dda3563f7ddf3d247fe
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2022-05-22 12:32:20 -0300

    teste

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from dash import Dash, html, dcc, Input, Output, callback
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
+import flask
 
 # Import all application pages
 from pages import home, page1, page2
 
+server = flask.Flask(__name__)
 # Add boostrap theme in 'external_stylesheets'
 app = Dash(__name__, external_stylesheets=[dbc.themes.COSMO], suppress_callback_exceptions=True)
 
